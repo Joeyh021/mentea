52d08a0c6d96143c62f081fc9d74b89f55788b8c
Joe Harrison <josephharrison1@me.com>
2022-01-13 20:06:32 +0000

    disallow untyped test defs

--- test/test_example.py
+++ test/test_example.py
@@ -1,9 +1,10 @@
-"""testing everything in this file"""
+"""testing everything in the python_example module"""
 
 import python_example
 
 
-def test_funcs():
+def test_funcs() -> None:
+    """test the functions in the example"""
     assert python_example.square(2) == 4
     assert python_example.is_one(1)
     assert python_example.isnt_one(2)
