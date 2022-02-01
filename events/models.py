981acc1a6948a10fed2002555a5f54ef22dfa482
Josh Davies <Joshua.T.Davies@warwick.ac.uk>
2022-02-01 14:25:17 +0000

    Update models following ERD changes

--- events/models.py
+++ events/models.py
@@ -24,9 +24,52 @@ class EventType:
 
 class EventAttendee:
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    attendee = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, db_index=True)
+    attendee = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
+
+
+class EventRequest:
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    type = models.ForeignKey(EventType, on_delete=models.CASCADE)
+    requested_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    associated_topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
 
 
 class FeedbackForm:
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+
+
+class Questions:
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    type = models.ForeignKey(QuestionType, on_delete=models.CASCADE)
+    data = models.JSONField()
+    form = models.ForeignKey(FeedbackForm, on_delete=models.CASCADE)
+    order = models.IntegerField()
+    required = models.BooleanField()
+
+
+class QuestionType:
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    type_data = models.JSONField()
+
+
+class Answer:
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    associated_question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    associated_submission = models.ForeignKey(
+        FeedbackSubmission, on_delete=models.CASCADE
+    )
+    data = models.CharField(max_length=200)
+
+
+class FeedbackSubmission:
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    form = models.ForeignKey(FeedbackForm, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- people/models.py
+++ people/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class User:
+    # Main user model
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -15,13 +16,27 @@ class User:
 
 
 class UserType:
+    # Mentor or mentee
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     type = models.CharField(max_length=50)
 
 
 class BusinessArea:
+    # Business areas within the company
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     business_area = models.CharField(max_length=50)
+
+
+class MentorMentee:
+    # Mentor-mentee relationship
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    mentor = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="mentor", db_index=True
+    )
+    mentee = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="mentee", db_index=True
+    )
+    approved = models.BooleanField(default=False)
 
 
 class Notification:
@@ -33,24 +48,84 @@ class Notification:
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-class Theme:
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    theme = models.CharField(max_length=50)
-
-
 class Topic:
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     topic = models.CharField(max_length=50)
-    theme = models.ForeignKey(Theme, on_delete=models.CASCADE)
-
-
-class UserTheme:
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    theme = models.ForeignKey(Theme, on_delete=models.CASCADE)
 
 
 class UserTopic:
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, db_index=True)
+
+
+class Rating:
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    mentor = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="mentor_rating", db_index=True
+    )
+    rating = models.IntegerField()
+    associated_topic = models.ForeignKey(Topic, on_delete=models.CASCADE, db_index=True)
+    rated_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="rated_by",
+        db_index=True,
+        blank=True,
+        null=True,
+    )
+
+
+class PlanOfAction:
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    associated_mentor = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="plan_of_action_mentor",
+        db_index=True,
+    )
+    associated_mentee = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="plan_of_action_mentee",
+        db_index=True,
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class PlanOfActionTarget:
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    achieved_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    associated_poa = models.ForeignKey(
+        PlanOfAction, on_delete=models.CASCADE, db_index=True
+    )
+    set_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, db_index=True, blank=True, null=True
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Chat:
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    mentor = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="chat_mentor", db_index=True
+    )
+    mentee = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="chat_mentee", db_index=True
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ChatMessage:
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, db_index=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
+    content = models.CharField(max_length=200)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
