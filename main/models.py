09c8b0ce6d23753fbace6063add7bf315bf80ae4
Noah Hollowell <noahdhollowell@googlemail.com>
2022-02-04 12:06:58 +0000

    Switched over document to use ContentType for polymorphic relation Above needs testing, onlt hade time to implement, not run

--- main/models.py
+++ main/models.py
@@ -1,18 +1,22 @@
 import uuid
 
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
 class Document:
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
     file = models.FileField(upload_to="documents/")
-    associated_user = models.ForeignKey(  # Probably better as a polymorphi type
-        "User", on_delete=models.CASCADE, blank=True, null=True
+
+    associated_object_id = models.UUIDField()
+    associated_content_type = models.ForeignKey(
+        ContentType, on_delete=models.CASCADE, blank=True, null=True
     )
-    associated_event = models.ForeignKey(
-        "Event", on_delete=models.CASCADE, blank=True, null=True
-    )
+
+    associated = GenericForeignKey("associated_content_type", "associated_object_id")
+
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
