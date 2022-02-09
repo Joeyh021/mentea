136c4dcef3b9690667ff1342c6289f1aa434c8ee
Joe Harrison <josephharrison1@me.com>
2022-02-09 19:52:18 +0000

    Add some testing bits and bobs (#30)
    * update readme to reflect django bug
    
    * fucked with the testing packages and setup
    
    * added verbose flag to pytest run
    
    * added some example tests
    
    * split CI into jobs and added caching
    
    * Update CI.yml
    
    * fix syntax error
    
    * clearly i have greatly misunderstood how github actions works
    
    * The workflow is not valid. .github/workflows/CI.yml (Line: 6, Col: 3): The identifier 'lint and test' is invalid. IDs may only contain alphanumeric characters, '_', and '-'. IDs must start with a letter or '_' and and must be less than 100 characters.
    
    * aded condition for loading venv depending on cache hit

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test.client import Client
+from pytest_django import asserts
 
-# Create your tests here.
+
+def test_main_index_page(client: Client) -> None:
+    response = client.get("/")
+    asserts.assertTemplateUsed(response, "main/index.html")
+    # add some assertions about the template contexts here once those are done
+    # https://docs.djangoproject.com/en/4.0/topics/testing/tools/#assertions
+    # https://docs.djangoproject.com/en/4.0/topics/testing/tools/#testing-responses
+
+
+def test_faq_page(client: Client) -> None:
+    response = client.get("faq/")
+    asserts.assertTemplateNotUsed(response, "main/faq.html")
+    # TODO: change this from NotUsed when the template gets written
