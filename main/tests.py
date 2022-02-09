cda6171dc570ed7457ac7c5ccce387d2e2b00e14
Joe Harrison <josephharrison1@me.com>
2022-02-09 18:44:09 +0000

    added some example tests

--- main/tests.py
+++ main/tests.py
@@ -4,4 +4,13 @@ from pytest_django import asserts
 
 def test_main_index_page(client: Client) -> None:
     response = client.get("/")
-    asserts.assertTemplateUsed(response, "main/inde.html")
+    asserts.assertTemplateUsed(response, "main/index.html")
+    # add some assertions about the template contexts here once those are done
+    # https://docs.djangoproject.com/en/4.0/topics/testing/tools/#assertions
+    # https://docs.djangoproject.com/en/4.0/topics/testing/tools/#testing-responses
+
+
+def test_faq_page(client: Client) -> None:
+    response = client.get("faq/")
+    asserts.assertTemplateNotUsed(response, "main/faq.html")
+    # TODO: change this from NotUsed when the template gets written
