c44aaa34a04c2c96c4a7fdaa5a1c6fc0130c02d9
Piotrek <mroczny2210@wp.pl>
2022-02-11 13:12:36 +0000

    painted black

--- people/urls/user.py
+++ people/urls/user.py
@@ -4,8 +4,8 @@ from ..views import *
 
 
 urlpatterns = [
-    path("", include('django.contrib.auth.urls')),
-    #path("login/", UserLoginPage.as_view(), name="login"),
+    path("", include("django.contrib.auth.urls")),
+    # path("login/", UserLoginPage.as_view(), name="login"),
     path("register/", UserSignupPage.as_view(), name="register"),
     path("calendar/", UserCalendarPage.as_view(), name="calendar"),
     path("profile/", UserProfilePage.as_view(), name="profile"),
