de3e0433fde418979bd7ca4f80a0985f60bc5449
Piotrek <mroczny2210@wp.pl>
2022-02-11 12:54:47 +0000

    Added basic login and password change functions

--- people/urls/user.py
+++ people/urls/user.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import include, path
 
 from ..views import *
 
 
 urlpatterns = [
-    path("login/", UserLoginPage.as_view(), name="login"),
+    path("", include('django.contrib.auth.urls')),
+    #path("login/", UserLoginPage.as_view(), name="login"),
     path("register/", UserSignupPage.as_view(), name="register"),
     path("calendar/", UserCalendarPage.as_view(), name="calendar"),
     path("profile/", UserProfilePage.as_view(), name="profile"),
