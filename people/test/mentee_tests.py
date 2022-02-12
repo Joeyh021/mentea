8a3bdf92025cd6e77a876fbdf201a3d69cfef32c
Joe Harrison <josephharrison1@me.com>
2022-02-12 16:40:34 +0000

    add more test examples

---
+++ people/test/mentee_tests.py
@@ -0,0 +1,10 @@
+from django.test.client import Client
+from pytest_django import asserts
+
+
+def test_mentee_dashboard_page(client: Client) -> None:
+    response = client.get("mentee/dashboard")
+    asserts.assertTemplateUsed(response, "people/mentee_dashboard.html")
+    # TODO:  add some assertions about the template contexts here once those are done
+    # https://docs.djangoproject.com/en/4.0/topics/testing/tools/#assertions
+    # https://docs.djangoproject.com/en/4.0/topics/testing/tools/#testing-responses
