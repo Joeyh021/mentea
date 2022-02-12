a34b83406b48caeddcb39239ad239b9577b1c8b8
Joe Harrison <josephharrison1@me.com>
2022-02-12 16:52:20 +0000

    lets try that again shall we

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 # type: ignore
 from django.core.management import call_command
 import pytest
-from test.make_test_data import create_data
+from tests.make_test_data import create_data
 
 # populate the test database with some basic data from test_data.json
 
