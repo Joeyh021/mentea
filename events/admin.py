d2d74b13ee8956b205fb4a021ac39ab7b68546f0
Noah Hollowell <noahdhollowell@googlemail.com>
2022-02-12 17:38:14 +0000

    plz stop complaing about formatting *cries*

--- events/admin.py
+++ events/admin.py
@@ -1,22 +1,36 @@
 from django.contrib import admin
 
-from events.models import Answer, FeedbackForm, FeedbackSubmission, QuestionType, Questions
+from events.models import (
+    Answer,
+    FeedbackForm,
+    FeedbackSubmission,
+    QuestionType,
+    Questions,
+)
 
 # Register your models here.
 
+
 @admin.register(FeedbackForm)
 class AdminFeedbackForm(admin.ModelAdmin):
-    list_display=('name',)
+    list_display = ("name",)
     pass
+
+
 @admin.register(FeedbackSubmission)
 class AdminFeedbackSubmission(admin.ModelAdmin):
     pass
+
+
 @admin.register(Questions)
 class AdminQuestion(admin.ModelAdmin):
     pass
+
+
 @admin.register(QuestionType)
 class AdminQuestionType(admin.ModelAdmin):
-    list_display =('name',)
+    list_display = ("name",)
+
 
 @admin.register(Answer)
 class AdminAnswer(admin.ModelAdmin):
