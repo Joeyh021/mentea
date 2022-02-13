b11c84554a79ba47a64f54cea50566c99d949719
Joe Harrison <josephharrison1@me.com>
2022-02-13 16:13:17 +0000

    cry about it

--- people/migrations/0003_merge_20220213_1607.py
+++ people/migrations/0003_merge_20220213_1607.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('people', '0002_remove_usertopic_people_user_user_id_fe678a_idx_and_more'),
-        ('people', '0002_user_bio_alter_user_business_area_and_more'),
+        ("people", "0002_remove_usertopic_people_user_user_id_fe678a_idx_and_more"),
+        ("people", "0002_user_bio_alter_user_business_area_and_more"),
     ]
 
-    operations = [
-    ]
+    operations = []
