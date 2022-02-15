a8851a7ea48c3ee5bf2d21feaf579bb19adc242e
Joe Harrison <josephharrison1@me.com>
2022-02-15 22:20:43 +0000

    added debug mode

--- people/matching.py
+++ people/matching.py
@@ -1,12 +1,14 @@
-from typing import List, Tuple
-import uuid
+from typing import Any, List, Tuple, Union, Dict
 
 from people.models import *
 from events.models import Event
 
 
-def get_matches(mentee: User) -> List[User]:
-    """given the uuid of a mentee, find the best possible mentors, returning a list of their uuids in order of suitability"""
+def get_matches(mentee: User, debug: bool = False):
+    """
+    given the uuid of a mentee, find the best possible mentors, returning a list of their uuids in order of suitability
+    if debug, then a dict of the calculated values is returned for testing/debugging purposes
+    """
     mentor_type = UserType.objects.get(type="mentor")
 
     mentee_topics = [ut.topic for ut in UserTopic.objects.filter(user=mentee)]
@@ -16,7 +18,7 @@ def get_matches(mentee: User) -> List[User]:
         )
     )
 
-    mentor_scores: List[Tuple[User, float]] = []
+    mentor_scores = []
     for mentor in all_mentors:
         # get topic overlap
         mentor_topics = [ut.topic for ut in UserTopic.objects.filter(user=mentor)]
@@ -30,11 +32,9 @@ def get_matches(mentee: User) -> List[User]:
         # just use average of all for now - can maybe add more nuance later (TODO)
         # assume ratings are 0 <= r <= 5 to normalise 0 to 1
         if len(mentor_ratings) == 0:
-            average_rating = 0.0
+            average_rating = 1.0  # if no ratings then just ignore
         else:
             average_rating = sum(mentor_ratings) / len(mentor_ratings) / 5
-
-        score = average_rating * topic_overlap_score
 
         # calculate workload
         n_mentees = len(MentorMentee.objects.filter(mentor=mentor))
@@ -44,11 +44,30 @@ def get_matches(mentee: User) -> List[User]:
         workshop_workload_factor = 0.1
         workload = n_mentees + n_workshops * workshop_workload_factor
 
-        # score factore determines how much the score should be weighted
-        score_factor = 5
-        score += workload + score * score_factor
+        score = 1.0  # some computation
 
-        mentor_scores.append((mentor, score))
+        if debug:
+            mentor_scores.append(
+                (
+                    mentor,
+                    {
+                        "topic overlap": topic_overlap,
+                        "topic overlap score": topic_overlap_score,
+                        "ratings": mentor_ratings,
+                        "avg rating": average_rating,
+                        "mentees": n_mentees,
+                        "workshops": n_workshops,
+                        "workload": workload,
+                        "final score": score,
+                    },
+                )
+            )
+        else:
+            mentor_scores.append((mentor, score))
 
-    mentor_scores.sort(key=lambda t: t[1], reverse=True)
-    return [t[0] for t in mentor_scores]
+    if debug:
+        mentor_scores.sort(key=lambda t: t[1]["final score"], reverse=True)
+        return [t[0] for t in mentor_scores]
+    else:
+        mentor_scores.sort(key=lambda t: t[1], reverse=True)
+        return [t[0] for t in mentor_scores]
