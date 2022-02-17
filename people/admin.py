05b124fdc7b530c950576edc33beebf63fd9cd7d
Josh Davies <Joshua.T.Davies@warwick.ac.uk>
2022-02-17 22:37:51 +0000

    Show our custom user model in Django admin

--- people/admin.py
+++ people/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+import django_stubs_ext
+
+django_stubs_ext.monkeypatch()
+
+from .models import *
 
 # Register your models here.
+
+@admin.register(User)
+class AdminUser(admin.ModelAdmin[User]):
+    pass
