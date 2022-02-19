0ca1488341f025dc87d5cc9a46a59f6e5d6dc2d0
Joe Harrison <josephharrison1@me.com>
2022-02-19 14:15:34 +0000

    first two tests done

--- people/test/matching_tests.py
+++ people/test/matching_tests.py
@@ -16,13 +16,51 @@ def test_matching_data() -> None:
 
 @pytest.mark.dependency(depends=["test_matching_data"])
 def test_matches_1() -> None:
-    """Test the matches are returned in the order expected"""
+    """Test the matches are returned in the order expected with the scores expected"""
+    # testing tim here
     tim = User.objects.get(first_name="Tim", last_name="Mentee")
     matches = get_matches(tim)
 
-    # steve should be the top match because the topics overlap
-    assert matches[0] == User.objects.get(first_name="Steve")
-    assert len(matches) == 2
+    # overlap score is 1.0
+    # minus 0.125 because steve already has one mentor (Joey)
+    assert matches[0] == (User.objects.get(first_name="Steve"), pytest.approx(0.875))
+    # should be 3 matches total - all those not in the money laundering department
+    assert len(matches) == 3
 
 
-# maybe like a couple of these
+@pytest.mark.dependency(depends=["test_matching_data"])
+def test_matches_2() -> None:
+    """Test the matches are returned in the order expected with the scores expected"""
+    # testing sandra here
+    sandra = User.objects.get(first_name="Sandra")
+    matches = get_matches(sandra)
+
+    # gonna look at top two with overlap - john and joey
+    # john has an overlap score of 0.25
+    # joey has an overlap score of 0.5
+    # joey also has terrible reviews, bringing his score down to 0.1
+    # joey also has a mentee already, bringing his score down further to 0.0875
+    # minus 0.125 because john already has one mentor (Joey)
+    assert matches[0] == (User.objects.get(first_name="John"), pytest.approx(0.25))
+    assert matches[1] == (User.objects.get(first_name="Joey"), pytest.approx(0.0875))
+    # should be 5 matches total - no one else works in noncing at the moment
+    assert len(matches) == 5
+
+
+@pytest.mark.dependency(depends=["test_matching_data"])
+def test_matches_3() -> None:
+    """Test the matches are returned in the order expected with the scores expected"""
+    # testing sandra here
+    sandra = User.objects.get(first_name="Sandra")
+    matches = get_matches(sandra)
+
+    # gonna look at top two with overlap - john and joey
+    # john has an overlap score of 0.25
+    # joey has an overlap score of 0.5
+    # joey also has terrible reviews, bringing his score down to 0.1
+    # joey also has a mentee already, bringing his score down further to 0.0875
+    # minus 0.125 because john already has one mentor (Joey)
+    assert matches[0] == (User.objects.get(first_name="John"), pytest.approx(0.25))
+    assert matches[1] == (User.objects.get(first_name="Joey"), pytest.approx(0.0875))
+    # should be 5 matches total - no one else works in noncing at the moment
+    assert len(matches) == 5
