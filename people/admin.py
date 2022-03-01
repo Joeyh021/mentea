5922689ab4419244db009c5333ce2299adf31635
Noah Hollowell <noahdhollowell@googlemail.com>
2022-03-01 14:39:16 +0000

    Fixed create plan formatting

--- people/admin.py
+++ people/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import BusinessArea, Notification, Topic, User, UserType
+from .models import BusinessArea, MentorMentee, Notification, Topic, User, UserType
 
 # Register your models here.
 
@@ -9,3 +9,4 @@ admin.site.register(User)
 admin.site.register(BusinessArea)
 admin.site.register(Topic)
 admin.site.register(Notification)
+admin.site.register(MentorMentee)
