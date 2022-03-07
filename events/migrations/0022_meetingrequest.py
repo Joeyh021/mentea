bb237ef43df3b3ba1a2f14473cf4203ca097b461
TeoMili <teomili@hotmail.com>
2022-03-07 10:43:51 +0000

    poetry run black

--- events/migrations/0022_meetingrequest.py
+++ events/migrations/0022_meetingrequest.py
@@ -10,18 +10,37 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('events', '0021_remove_eventrequest_events_even_request_beb3ea_idx_and_more'),
+        ("events", "0021_remove_eventrequest_events_even_request_beb3ea_idx_and_more"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='MeetingRequest',
+            name="MeetingRequest",
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ('mentor_approved', models.BooleanField()),
-                ('mentee_approved', models.BooleanField()),
-                ('event', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='events.event')),
-                ('mentee', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("mentor_approved", models.BooleanField()),
+                ("mentee_approved", models.BooleanField()),
+                (
+                    "event",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="events.event"
+                    ),
+                ),
+                (
+                    "mentee",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
     ]
