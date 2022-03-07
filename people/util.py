0a192b6b267cc9b14eff06cc04e9bb7aa5654605
Lukas Miller <Luke.Miller@warwick.ac.uk>
2022-03-07 19:53:14 +0000

    Removed the get_meeting_request util function, was a stupid idea

--- people/util.py
+++ people/util.py
@@ -6,7 +6,3 @@ from datetime import datetime
 def get_mentor(mentee: User) -> User:
     relationship = MentorMentee.objects.get(mentee=mentee)
     return User.objects.get(id=relationship.mentor.id)
-
-def get_meeting_request(mentee: User, start_time: datetime) -> Event:
-    event = Event.objects.get(mentee=mentee, start_time=start_time)
-    return MeetingRequest.objects.get(eventid=event.id)
