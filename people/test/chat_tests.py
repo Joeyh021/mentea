90e19db735f9fcef5a545efa76fc5851c0420e8b
Joey <josephharrison1@me.com>
2022-03-12 00:50:24 +0000

    added chat test (failing)

--- people/test/chat_tests.py
+++ people/test/chat_tests.py
@@ -1,3 +1,4 @@
+from email import message
 from django.test.client import Client
 from pytest_django import asserts
 import pytest
@@ -6,11 +7,35 @@ from people.models import *
 pytestmark = pytest.mark.django_db()
 
 
-def test_mentor_send_chat(client: Client, mentor: User):
-    """Test a mentor can send a chat and mentee recieve it"""
-    pass
+def test_send_and_receive_chat(client: Client):
+    """Test a mentor and mentee can send and recieve chat"""
+    # login manually so we can log in and out of mentee and mentor
+    client.login(email="mentee@mentea.me", password="menteepass")
+    mentee = User.objects.get(email="mentee@mentea.me")
 
+    # test can see chat page
+    response = client.get("/mentee/chat/")
+    # can we see the messages already there?
+    messages = list(response.context["chatMessages"])
+    assert messages[0].content == "What is your favourite colour?"
+    assert messages[1].sender == mentee
 
-def test_mentee_send_chat(client: Client, mentor: User):
-    """Same as above but vice-versa"""
-    pass
+    # send new message
+    client.post("/mentee/chat/", data={"content": "Wait, no, yellow!"})
+    # can we see it?
+    response = client.get("/mentee/chat/")
+    assert list(response.context["chatMessages"])[2].content == "Wait, no, yellow!"
+
+    client.logout()
+    client.login(email="mentor@mentea.me", password="mentorpass")
+    mentor = User.objects.get(email="mentor@mentea.me")
+
+    # can the mentor see it?
+    response = client.get(f"/mentor/{mentee.id}/chat/")
+    messages = list(response.context["chatMessage"])
+    assert [m.content for m in messages] == [
+        "What is your favourite colour?",
+        "Blue",
+        "Wait, no, yellow!",
+    ]
+    assert messages[2].sender == mentee
