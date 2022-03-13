5c87009449a199d3de6c190dc5f839efdb5025b3
Josh Davies <Joshua.T.Davies@warwick.ac.uk>
2022-03-13 17:38:00 +0000

    Use console backend for emails as default
    Can be overriden if needed, just for testing

--- mentea/settings/base.py
+++ mentea/settings/base.py
@@ -141,3 +141,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Email backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
